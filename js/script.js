5909216e1fd4a46aaa8dba63350b898a50c096e8
portal-saojose2025 <tecnicogigabit.1832@gmail.com>
2025-08-23 14:06:58 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+const fotos = [
+  "assets/img/foto1.jpg",
+  "assets/img/foto2.jpg",
+  "assets/img/foto3.jpg"
+];
+
+const videos = [
+  "assets/videos/video1.mp4",
+  "assets/videos/video2.mp4"
+];
 
